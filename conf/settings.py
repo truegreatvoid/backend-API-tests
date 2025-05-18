d3ae4262af5b764714bc58e2069b4d5bb0f71d87
Darlan0307 <darlanchagas2020@gmail.com>
2025-05-18 14:10:16 -0300

    habilitando cors

--- conf/settings.py
+++ conf/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
 
     'drf_spectacular',
     'rest_framework',
+    'corsheaders',
 
     'apps.core',
     'apps.api',
@@ -36,12 +37,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'conf.urls'
 
